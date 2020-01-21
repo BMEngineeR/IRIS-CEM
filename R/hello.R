bb42a3956c62de803b459402d2928e5731bb1427
BMEngineeR <cyz931123@gmail.com>
2020-01-20 23:41:10 -0500

    fix bug missing column

--- R/hello.R
+++ R/hello.R
@@ -1,14 +1,29 @@
-# setwd("c:/analysis_work/BRIC/")
-# a<- read.csv("C:/Users/cha224/Documents/BRIC/inst/extdata/Pollen/Pollen_expression.csv",header = T,row.names = 1)
-# object<-CreateBRICObject(as.matrix(a))
-# object <- BRIC::NormalizeData(object)
-# object <- RunDiscretization(object)
+library(BRIC)
+setwd("D:/my_analysis/BRIC_TEST/")
+a<- read.csv("C:/Users/cyz/Documents/BRIC/BRIC/inst/extdata/Pollen/Pollen_expression.csv",header = T,row.names = 1)
+object<-CreateBRICObject(as.matrix(a))
+object <- BRIC::NormalizeData(object)
+object <- RunDiscretization(object)
 # # LTMG suite
 # object <- BRIC::RunLTMG(object,Gene_use = 2000)
 # object <- BRIC::CalBinaryMultiSignal(object)
 # object <- BRIC::CalBinarySingleSignal(object)
-# start.time <- Sys.time()
-# object <- BRIC::RunBicluster(object = object,DiscretizationModel = "LTMG")
-# end.time <- Sys.time()
-# totAL <- start.time - end.time
-#
+start.time <- Sys.time()
+BRIC::RunBicluster(object = object,DiscretizationModel = "Quantile")
+end.time <- Sys.time()
+start.time - end.time
+
+# test large cell
+setwd("D:/my_analysis/BRIC_TEST/")
+a <- ReadFrom10X_folder("C:/Users/cyz/Documents/BRIC/BRIC/inst/extdata/10X_3K/folder_10X/")
+object<-CreateBRICObject(as.matrix(a))
+object@processed_count <- as.matrix(a)
+object <- RunDiscretization(object)
+# # LTMG suite
+# object <- BRIC::RunLTMG(object,Gene_use = 2000)
+# object <- BRIC::CalBinaryMultiSignal(object)
+# object <- BRIC::CalBinarySingleSignal(object)
+start.time <- Sys.time()
+BRIC::RunBicluster(object = object,DiscretizationModel = "Quantile")
+end.time <- Sys.time()
+start.time - end.time
